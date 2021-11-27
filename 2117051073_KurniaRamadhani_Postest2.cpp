60c3f66b2481860518c511488f34026611adc7ee
Kurnia <92375799+KurniaRamadhani11@users.noreply.github.com>
2021-11-26 19:07:56 -0800

    Add files via upload

---
+++ 2117051073_KurniaRamadhani_Postest2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	//Kurnia Ramadhani 2117051073//
+	
+	int pena,buku,penghapus,penggaris,a,b,c,d;
+	
+	cout<<"Harga sebuah pena\t\t:";
+	cin>>a;
+	cout<<"Harga sebuah buku\t\t:";
+	cin>>b;
+	cout<<"Harga sebuah penghapus\t\t:";
+	cin>>c;
+	cout<<"Harga sebuah penggaris\t\t:";
+	cin>>d;
+	
+	cout<<"-------------------------------------"<<endl;
+	cout<<"Harga 73 buah pena\t\t= Rp."<<a*73<<endl;
+	cout<<"Harga 73 buah buku\t\t= Rp."<<b*73<<endl;
+	cout<<"Harga 73 buah penghapus\t\t= Rp."<<c*73<<endl;
+	cout<<"Harga 73 buah penggaris\t\t= Rp."<<d*73<<endl;
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
